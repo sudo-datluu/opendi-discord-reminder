25eb00fbf24062b34a195760ca172263f2649fef
datluu-dev <tad.louis2810@gmail.com>
2023-12-08 23:37:17 +1100

    add exclude users, add command line options

--- channel_reminder.py
+++ channel_reminder.py
@@ -1,12 +1,10 @@
 import discord
 import os
+import argparse
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
 
 load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID"))  # Convert to int as get_channel expects an int
 
 client = discord.Client(intents=discord.Intents.all())
 
@@ -18,26 +16,44 @@ async def on_ready():
     os._exit(0)
 
 async def send_msg_daily():
-    channel = client.get_channel(CHANNEL_ID)
+    channel = client.get_channel(DAILY_REPORT_CHANNEL_ID)
     await channel.send("Helu I'm Kangaroo. Nhớ report hàng ngày nhé các bạn!!")
     inactive_users = await check_inactive_users()
     await send_inactive_users(inactive_users)
 
 async def check_inactive_users():
-    channel = client.get_channel(CHANNEL_ID)
-
+    channels = [client.get_channel(DAILY_REPORT_CHANNEL_ID), client.get_channel(GENERAL_CHANNEL_ID)]
     active_users = set()
     one_day_ago = datetime.now() - timedelta(days=1) - timedelta(minutes=5)
-    async for message in channel.history(after=one_day_ago):
-        active_users.add(message.author)
+
+    for channel in channels:
+        async for message in channel.history(after=one_day_ago):
+            active_users.add(message.author)
+    
     all_channel_members = set(channel.members)
     inactive_users = all_channel_members - active_users
     return inactive_users
 
 async def send_inactive_users(inactive_users):
-    channel = client.get_channel(CHANNEL_ID)
-    mentions = ' '.join(user.mention for user in inactive_users)
-    # print(mentions)
-    await channel.send(f'Này các vị {mentions} chưa report nhé!! Đừng tưởng nhờn với Kangaroo đâu!!')
+    channel = client.get_channel(DAILY_REPORT_CHANNEL_ID)
+    mention_users = [user for user in inactive_users if user.name not in OFF_USERS and not user.bot]
+    if mention_users:
+        mentions = ' '.join(user.mention for user in mention_users)
+        await channel.send(f'Này các vị {mentions} chưa report nhé!! Đừng tưởng nhờn với Kangaroo đâu!!')
 
-client.run(BOT_TOKEN)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--test-mode', dest='test_mode', action='store_true')
+    parser.set_defaults(test_mode=False)
+
+    args = parser.parse_args()
+    
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+    # Convert to int as get_channel expects an int
+    DAILY_REPORT_CHANNEL_ID = int(os.getenv("DAILY_REPORT_CHANNEL_ID")) if not args.test_mode else  int(os.getenv("DAILY_REPORT_TEST_CHANNEL_ID"))
+    GENERAL_CHANNEL_ID = int(os.getenv("GENERAL_CHANNEL_ID"))  
+
+    OFF_USERS = ['phoebe0830']
+    client.run(BOT_TOKEN)
+    
